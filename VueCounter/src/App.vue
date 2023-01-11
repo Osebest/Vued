bd3315e75149555fababd132718861127d6b520a
Osebest <udazi.osebest@gmail.com>
2023-01-11 13:20:16 +0100

    Finished with the counter

---
+++ VueCounter/src/App.vue
@@ -0,0 +1,38 @@
+<script setup>
+  import { ref } from 'vue'
+
+  const count = ref(0)
+
+  const increment = () => {
+    count.value++
+  }
+
+  const decrement = () => {
+    count.value--
+  }
+</script>
+
+<template>
+  <main>
+    <div>
+      <h4>The current count is...</h4>
+      <h1>{{ count }}</h1>
+      <button @click="decrement">-</button>
+      <button @click="increment">+</button>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+  main{
+    background-color: aliceblue;
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  div{
+    text-align: center;
+  }
+</style>
